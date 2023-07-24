7488cf5c84e725fda2a06a2b964922e9631c10c4
Sylvester <mintahsylvesterkojo@gmail.com>
2023-07-24 18:20:35 +0000

    updated project

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons } from '@expo/vector-icons';
+import BackgroundImage from '../Components/Background';
 
 const ProfileScreen = () => {
   const handleLogout = () => {
@@ -9,41 +10,37 @@ const ProfileScreen = () => {
   };
 
   return (
-    <LinearGradient
-      colors={['#282D2E', '#282D2E']}
-      start={{ x: 0, y: 0 }}
-      end={{ x: 1, y: 1 }}
-      style={styles.container}
-    >
-      <Text style={styles.title}>My Profile</Text>
+      <BackgroundImage source={require('../assets/img.jpg')}>
+        <Text style={styles.title}>My Profile</Text>
 
-      <View style={styles.gridContainer}>
-        <TouchableOpacity style={styles.card}>
-          <Ionicons name="receipt" size={40} color="white" />
-          <Text style={styles.cardText}>My Bill</Text>
-        </TouchableOpacity>
+        <View style={styles.gridContainer}>
+          <TouchableOpacity style={styles.card}>
+            <Ionicons name="receipt" size={40} color="yellow" />
+            <Text style={styles.cardText}>My Bill</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.card}>
-          <Ionicons name="ios-lock-closed" size={40} color="white" />
-          <Text style={styles.cardText}>Security</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.card}>
+            <Ionicons name="ios-lock-closed" size={40} color="yellow" />
+            <Text style={styles.cardText}>Security</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.card}>
-          <Ionicons name="ios-help-circle" size={40} color="white" />
-          <Text style={styles.cardText}>FAQ</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.card}>
+            <Ionicons name="ios-help-circle" size={40} color="yellow" />
+            <Text style={styles.cardText}>FAQ</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.card}>
-          <Ionicons name="ios-call" size={40} color="white" />
-          <Text style={styles.cardText}>Contact Us</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.card}>
+            <Ionicons name="ios-call" size={40} color="yellow" />
+            <Text style={styles.cardText}>Contact Us</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.card} onPress={handleLogout}>
-          <Ionicons name="ios-log-out" size={40} color="white" />
-          <Text style={styles.cardText}>Log Out</Text>
-        </TouchableOpacity>
-      </View>
-    </LinearGradient>
+          <TouchableOpacity style={styles.card} onPress={handleLogout}>
+            <Ionicons name="ios-log-out" size={40} color="yellow" />
+            <Text style={styles.cardText}>Log Out</Text>
+          </TouchableOpacity>
+        </View>
+      </BackgroundImage>
+      
   );
 };
 
